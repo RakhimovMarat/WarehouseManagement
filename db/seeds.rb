924927f4f17136c7b496fc435984b7e9f76fcdd0
marat <mf.rakhimov1982@gmail.com>
2024-06-17 20:03:48 +0300

    Add gem Devise and user authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create({
+  username: 'admin',
+  email:    'admin@admin.com',
+  password: '123456',
+  admin:    true
+})
