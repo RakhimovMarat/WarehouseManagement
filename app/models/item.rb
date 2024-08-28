24ed3d633d9a69773aba81e3cd6e181a42be538d
marat <mf.rakhimov1982@gmail.com>
2024-08-28 20:47:16 +0300

    Create new logic adding item to address, update views

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
-  validates :number, presence: true, uniqueness: true
+  validates :number, presence: true
+  validates :number, uniqueness: { scope: :address_id }
+
   validates :description, presence: true
 
   belongs_to :address, optional: true
--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,19 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+   root "items#index"
 
   authenticate :user do
     resources :users
     resources :warehouses do
-      get :addresses, on: :member
+      get :addresses,             on: :member
+      get :information,           on: :member
+      get :get_item,              on: :member
+      patch :add_item_to_address, on: :member
     end
     resources :addresses
     resources :items do
-      get :get_address, on: :member
+      get :get_address,           on: :member
       patch :put_item_on_address, on: :member
     end
   end
